2d63cea9a98991827b95ad821a2ddb16460c2d40
André Eriksson <andreeriksson444@gmail.com>
2025-11-30 17:51:01 +0100

    feat: Storage subsystem
    Currently only has the directories.

---
+++ storage/src/paths.rs
@@ -0,0 +1,15 @@
+use std::path::PathBuf;
+
+use constants::APP_NAME;
+
+pub fn get_cache_path() -> Option<PathBuf> {
+    dirs::cache_dir().map(|p| p.join(APP_NAME))
+}
+
+pub fn get_config_path() -> Option<PathBuf> {
+    dirs::config_dir().map(|p| p.join(APP_NAME))
+}
+
+pub fn get_data_path() -> Option<PathBuf> {
+    dirs::data_dir().map(|p| p.join(APP_NAME))
+}
