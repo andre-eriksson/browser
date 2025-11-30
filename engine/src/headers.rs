26901ff27e623e2de4ceb01fd0cea56161c7ad8d
André Eriksson <andreeriksson444@gmail.com>
2025-11-30 17:57:34 +0100

    patch: Disabled UI
    Added more setup variables for the UI

---
+++ engine/src/headers.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+
+use constants::APP_NAME;
+use http::{
+    HeaderMap, HeaderValue,
+    header::{ACCEPT, ACCEPT_ENCODING, ACCEPT_LANGUAGE, CACHE_CONTROL, CONNECTION, USER_AGENT},
+};
+
+pub fn create_default_browser_headers() -> HeaderMap {
+    let mut browser_headers = HeaderMap::new();
+
+    let mut headers = HashMap::new();
+    headers.insert(ACCEPT, "text/html".to_string());
+    headers.insert(ACCEPT_ENCODING, "gzip, deflate, br".to_string());
+    headers.insert(
+        ACCEPT_LANGUAGE,
+        "en-US, sv-SE;q=0.9, en;q=0.8, sv;q=0.7".to_string(),
+    );
+    headers.insert(CACHE_CONTROL, "no-store".to_string());
+    headers.insert(CONNECTION, "keep-alive".to_string());
+
+    let user_agent = format!(
+        "Mozilla/5.0 (X11; Linux x86_64; rv:0.1) MyRenderer/0.1 {}/0.1",
+        APP_NAME
+    );
+    headers.insert(USER_AGENT, user_agent);
+
+    for (key, value) in headers {
+        if let Ok(header_value) = HeaderValue::from_str(&value) {
+            browser_headers.insert(key, header_value);
+        } else {
+            eprintln!(
+                "Failed to create header value for {}: {}",
+                key.as_str(),
+                value
+            );
+        }
+    }
+
+    browser_headers
+}
--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,10 +1,20 @@
+pub mod headers;
+
+use cookies::cookie_store::CookieJar;
 use tracing::Level;
 
-use ui::runtime::UiRuntime;
+use crate::headers::create_default_browser_headers;
+
+//use ui::runtime::UiRuntime;
 
 /// The main entry point for the application
 fn main() {
     tracing_subscriber::fmt().with_max_level(Level::INFO).init();
 
-    UiRuntime::run();
+    let _browser_headers = create_default_browser_headers();
+
+    // TODO: Load cookies from persistent storage
+    let _cookie_jar = CookieJar::new();
+
+    //UiRuntime::run();
 }
