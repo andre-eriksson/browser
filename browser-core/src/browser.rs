e38757e0380cdd7a76b9cea3cdb11c12f68377fc
André Eriksson <andreeriksson444@gmail.com>
2025-12-25 23:27:32 +0100

    feat: More browser commands

--- browser-core/src/browser.rs
+++ browser-core/src/browser.rs
@@ -6,7 +6,6 @@ use html_parser::{
     parser::HtmlStreamParser,
     state::{BlockedReason, ParserState},
 };
-use html_syntax::collector::DefaultCollector;
 use http::HeaderMap;
 use network::http::{client::HttpClient, request::RequestBuilder};
 use tracing::debug;
@@ -24,13 +23,15 @@ pub trait Commandable {
 }
 
 pub struct Browser {
+    active_tab: TabId,
+    tabs: Vec<Tab>,
+    next_tab_id: usize,
+
     emitter: Box<dyn Emitter<BrowserEvent> + Send + Sync>,
 
     http_client: Box<dyn HttpClient>,
     cookie_jar: Arc<Mutex<CookieJar>>,
     headers: Arc<HeaderMap>,
-
-    tabs: Vec<Tab>,
 }
 
 impl Browser {
@@ -41,23 +42,25 @@ impl Browser {
         headers: Arc<HeaderMap>,
     ) -> Self {
         Browser {
+            active_tab: TabId(0),
+            tabs: vec![Tab::new(TabId(0), None)],
+            next_tab_id: 1,
             emitter,
             http_client,
             cookie_jar,
             headers,
-            tabs: vec![Tab::new(0, None)],
         }
     }
 
-    pub fn print_body(&self, tab_id: usize) {
+    pub fn print_body(&self, tab_id: TabId) {
         if let Some(tab) = self.tabs.iter().find(|t| t.id == tab_id) {
             if let Some(document) = &tab.document {
-                debug!("Tab {} Document:\n{}", tab_id, document);
+                debug!("Tab {:?} Document:\n{}", tab_id, document);
             } else {
-                debug!("Tab {} has no document loaded.", tab_id);
+                debug!("Tab {:?} has no document loaded.", tab_id);
             }
         } else {
-            debug!("Tab {} does not exist.", tab_id);
+            debug!("Tab {:?} does not exist.", tab_id);
         }
     }
 
@@ -75,7 +78,7 @@ impl Commandable for Browser {
     async fn execute(&mut self, command: BrowserCommand) -> Result<BrowserEvent, String> {
         match command {
             BrowserCommand::Navigate { tab_id, url } => {
-                debug!("Navigating tab {} to URL: {}", tab_id, url);
+                debug!("Navigating tab {:?} to URL: {}", tab_id, url);
 
                 // TODO: Handle exceptions like about:blank or about:config
                 let url = match Url::parse(&url) {
@@ -84,7 +87,7 @@ impl Commandable for Browser {
                 };
 
                 if !self.tabs.iter().any(|t| t.id == tab_id) {
-                    return Err(format!("Tab with ID {} does not exist", tab_id));
+                    return Err(format!("Tab with ID {:?} does not exist", tab_id));
                 }
 
                 let request = RequestBuilder::from(url.clone()).build();
@@ -103,7 +106,11 @@ impl Commandable for Browser {
                     None => return Err("Response body is empty".to_string()),
                 };
 
-                let mut parser = HtmlStreamParser::<_, DefaultCollector>::simple(body.as_slice());
+                let mut parser = HtmlStreamParser::<_, TabCollector>::new(
+                    body.as_slice(),
+                    None,
+                    Some(TabCollector::default()),
+                );
 
                 loop {
                     parser.step()?;
@@ -177,7 +184,7 @@ impl Commandable for Browser {
                             }
                             _ => {
                                 debug!(
-                                    "Parser for tab {} is blocked for reason: {:?}",
+                                    "Parser for tab {:?} is blocked for reason: {:?}",
                                     tab_id, reason
                                 );
                                 break;
@@ -195,12 +202,66 @@ impl Commandable for Browser {
                     .tabs
                     .iter_mut()
                     .find(|t| t.id == tab_id)
-                    .ok_or_else(|| format!("Tab with ID {} does not exist", tab_id))?;
+                    .ok_or_else(|| format!("Tab with ID {:?} does not exist", tab_id))?;
                 tab.document = Some(parser_result.dom_tree);
 
-                return Ok(BrowserEvent::NavigateSuccess);
+                let metadata = TabMetadata {
+                    tab_id,
+                    title: parser_result.metadata.title.unwrap_or(url.to_string()),
+                };
+
+                return Ok(BrowserEvent::NavigateSuccess(metadata));
             }
-            _ => unimplemented!("Command not implemented yet"),
+            BrowserCommand::AddTab { url } => {
+                let new_tab_id = TabId(self.next_tab_id);
+
+                if let Some(url) = url {
+                    let parsed_url = match Url::parse(&url) {
+                        Ok(u) => u,
+                        Err(e) => return Err(format!("Invalid URL: {}", e)),
+                    };
+                    let new_tab = Tab::new(new_tab_id, Some(parsed_url));
+                    self.tabs.push(new_tab);
+                } else {
+                    let new_tab = Tab::new(new_tab_id, None);
+                    self.tabs.push(new_tab);
+                }
+
+                self.next_tab_id += 1;
+                debug!("Added new tab with ID {:?}", new_tab_id);
+
+                return Ok(BrowserEvent::TabAdded(new_tab_id));
+            }
+            BrowserCommand::CloseTab { tab_id } => {
+                debug!("Closing tab with ID {:?}", tab_id);
+                if let Some(pos) = self.tabs.iter().position(|t| t.id == tab_id) {
+                    self.tabs.remove(pos);
+                    debug!("Closed tab with ID {:?}", tab_id);
+
+                    if self.active_tab == tab_id
+                        && let Some(first_tab) = self.tabs.first()
+                    {
+                        self.active_tab = first_tab.id;
+                        debug!("Changed active tab to {:?}", first_tab.id);
+                        self.emitter
+                            .emit(BrowserEvent::ActiveTabChanged(first_tab.id));
+                    }
+
+                    return Ok(BrowserEvent::TabClosed(tab_id));
+                } else {
+                    return Err(format!("Tab with ID {:?} does not exist", tab_id));
+                }
+            }
+            BrowserCommand::ChangeActiveTab { tab_id } => {
+                if !self.tabs.iter().any(|t| t.id == tab_id) {
+                    return Err(format!("Tab with ID {:?} does not exist", tab_id));
+                }
+
+                self.active_tab = tab_id;
+                debug!("Changed active tab to {:?}", tab_id);
+
+                return Ok(BrowserEvent::ActiveTabChanged(tab_id));
+            }
         }
     }
 }
