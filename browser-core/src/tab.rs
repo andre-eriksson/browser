2a05df20c351c14c4697d21802a49fb26f57896e
André Eriksson <andreeriksson444@gmail.com>
2025-12-25 23:27:16 +0100

    feat: Added more tab details

--- browser-core/src/tab.rs
+++ browser-core/src/tab.rs
@@ -1,16 +1,74 @@
-use html_syntax::dom::DocumentRoot;
+use html_syntax::{
+    collector::{Collector, TagInfo},
+    dom::DocumentRoot,
+    tag::{HtmlTag, KnownTag},
+};
 use url::Url;
 
+#[derive(Default)]
+pub struct TabCollector {
+    /// Indicates whether the parser is currently within the `<head>` section of the HTML document.
+    pub in_head: bool,
+
+    /// Indicates whether the parser is currently within a `<title>` tag.
+    pub in_title: bool,
+
+    /// The title of the tab, if available.
+    pub title: Option<String>,
+}
+
+impl Collector for TabCollector {
+    fn collect(&mut self, tag: &TagInfo) {
+        if *tag.tag == HtmlTag::Known(KnownTag::Head) {
+            self.in_head = true;
+            return;
+        }
+
+        if *tag.tag == HtmlTag::Known(KnownTag::Body) {
+            self.in_head = false;
+            return;
+        }
+
+        if !self.in_head {
+            return;
+        }
+
+        if *tag.tag == HtmlTag::Known(KnownTag::Title) {
+            self.in_title = true;
+        }
+
+        if self.in_title && tag.data.is_some() {
+            self.title = Some(tag.data.unwrap().clone());
+            self.in_title = false;
+        }
+    }
+
+    fn into_result(self) -> Self {
+        Self {
+            in_head: self.in_head,
+            in_title: self.in_title,
+            title: self.title,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct TabId(pub usize);
+
 pub struct Tab {
-    pub id: usize,
-
+    pub id: TabId,
     pub current_url: Option<Url>,
-
     pub document: Option<DocumentRoot>,
 }
 
+#[derive(Debug, Clone)]
+pub struct TabMetadata {
+    pub tab_id: TabId,
+    pub title: String,
+}
+
 impl Tab {
-    pub fn new(id: usize, url: Option<Url>) -> Self {
+    pub fn new(id: TabId, url: Option<Url>) -> Self {
         Tab {
             id,
             current_url: url,
