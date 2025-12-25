bd566a6426365f449d34cc68e9d9d230d7d8288c
André Eriksson <andreeriksson444@gmail.com>
2025-12-25 23:29:17 +0100

    delete: Event utility

--- utilities/event/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod browser;
-
-/// A trait representing an event emitter that can emit events of type `T`.
-pub trait Emitter<T>: Send + Sync {
-    fn emit(&self, event: T);
-    fn clone_box(&self) -> Box<dyn Emitter<T>>;
-}
