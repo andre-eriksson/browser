d9807ac22fe25f002ca90061ac1f586b7477867e
André Eriksson <andreeriksson444@gmail.com>
2026-01-23 22:04:43 +0100

    feat: Improved paths for Mac & Windows

--- crates/storage/src/paths.rs
+++ crates/storage/src/paths.rs
@@ -7,11 +7,31 @@ pub fn get_cache_path() -> Option<PathBuf> {
 }
 
 pub fn get_config_path() -> Option<PathBuf> {
-    dirs::config_dir().map(|p| p.join(APP_NAME))
+    let base_dir = dirs::config_dir()?.join(APP_NAME);
+
+    #[cfg(any(target_os = "macos", target_os = "windows"))]
+    {
+        Some(base_dir.join("Config"))
+    }
+
+    #[cfg(not(any(target_os = "macos", target_os = "windows")))]
+    {
+        Some(base_dir)
+    }
 }
 
 pub fn get_data_path() -> Option<PathBuf> {
-    dirs::data_dir().map(|p| p.join(APP_NAME))
+    let base_dir = dirs::data_dir()?.join(APP_NAME);
+
+    #[cfg(any(target_os = "macos", target_os = "windows"))]
+    {
+        Some(base_dir.join("Data"))
+    }
+
+    #[cfg(not(any(target_os = "macos", target_os = "windows")))]
+    {
+        Some(base_dir)
+    }
 }
 
 pub fn create_paths(path: &PathBuf) -> std::io::Result<()> {
